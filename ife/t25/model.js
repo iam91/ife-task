11ec81548ac7bcd672cb44d950c13861b354f289
iam91 <zhengwy91@foxmail.com>
2016-09-20 17:24:46 +0800

    bug on "this" solved

--- ife/t25/model.js
+++ ife/t25/model.js
@@ -23,19 +23,17 @@ function DirModel(rawData, view){
 	this._rawData = rawData;
 	this._root = null;
 	DirModel.prototype.init = function(){
-		var rootView = DirModel.prototype.build(this._rawData, this._view);
+		var rootView = DirModel.prototype._build.call(this, this._rawData);
 		this._view.show(rootView.getViewNode());
-		console.log(this);
 	};
-	DirModel.prototype.build = function(data, view){
-		console.log(this);
+	DirModel.prototype._build = function(data, view){
 		var modelNode = new ModelNode(data);
-		var newViewNode = view.renderNewDir(data.val);
+		var newViewNode = this._view.renderNewDir(data.val);
 		modelNode.setViewNode(newViewNode);
 		for(var i = 0; i < data.children.length; i++){
-			var childModelNode = DirModel.prototype.build(data.children[i], view);
+			var childModelNode = DirModel.prototype._build.call(this, data.children[i]);
 			modelNode.children.push(childModelNode);
-			view.appendChild(newViewNode, childModelNode.getViewNode());
+			this._view.appendChild(newViewNode, childModelNode.getViewNode());
 		}
 		return modelNode;
 	};
