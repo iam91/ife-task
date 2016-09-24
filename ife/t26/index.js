6289acda7c5d5103fd8aace536c892efd929c033
iam91 <zhengwy91@foxmail.com>
2016-09-24 14:31:25 +0800

    update

--- ife/t26/index.js
+++ ife/t26/index.js
@@ -1,105 +1,271 @@
 var $$ = function(element){
 	//convert newly created dom object to jquery object
 	return $(document.createElement(element));
-}
+};
+
+var sysTimeInterval = 50;
 
 /*
 
-|--- ship --- dom
-		 |--- position
-         |--- fuel
-         |--- state
-         |--- subjectiveId
-         |--- objectiveId
-         |*** move()
-         |*** stop()
-         |*** consumeFuel()
-         |*** provideFuel()
-         |*** destroy()
+|--- ship ---|
 
 */
 
-function Ship(space){
-	var _ = Ship.prototype;
-
-	this._space = space;
+function Ship(id){
+	this._space = $('.space');
+	this._id = id;
 	this._position = 0;
 	this._speed = 8;
+	this._stepInterval = 50;
+	this._fuel = 100;
 	this._isRunning = false;
+	this._isCharging = false;
+	
+	this._fd = -1;
+	this.chargeFd = -1;
+	this.fuelRenderFd = -1;
+
 	this._dom = (function(){
-		var orbit = $$('div').addClass('orbit').appendTo(this.space);
-		$$('div').addClass('ship').appendTo(orbit);
+		var self = this;
+		var charge = function(){
+			self.charge();
+		};
+		var fuelRender = function(){
+			self.fuelRender();
+		};
+		this._chargeFd = setInterval(charge, this._param.stepInterval);
+		this._fuelRenderFd = setInterval(fuelRender, this._param.fuelRenderInterval);
+
+		var orbit = $$('div').addClass('orbit')
+							 .html(Ship.prototype.template.replace(/\{\}/g, this._id)
+							 							  .replace(/\[\]/g, this._fuel))
+							 .appendTo(this._space);
 		return orbit;
 	}).call(this);
 }
 
+Ship.prototype._param = {
+	fuelRenderInterval: 1000,
+	stepInterval: sysTimeInterval,
+	speed: 12 / (1000 / sysTimeInterval), //12 deg/s
+	consume: 20 / (1000 / sysTimeInterval),
+	charge: 10 / (1000 / sysTimeInterval),
+	fuelPerStep: 50 / (1000 / sysTimeInterval)
+};
+
+Ship.prototype.template = 
+	"<div class=ship>{}号-[]%</div>";
+
+Ship.prototype.fuelRender = function(){
+	this._dom.html(Ship.prototype.template.replace(/\{\}/g, this._id)
+							 			  .replace(/\[\]/g, parseInt(this._fuel)));
+}
+
+Ship.prototype.destroy = function(){
+	this.stop();
+	this._dom.remove();
+	this._space = null;
+	clearInterval(this._chargeFd);
+	clearInterval(this._fuelRenderFd);
+};
+
+Ship.prototype.consume = function(){
+	var curr = this._fuel;
+	this._fuel -= (curr - this._param.consume < 0) ? 0 : this._param.consume;
+};
+
+Ship.prototype.charge = function(){
+	var curr = this._fuel;
+	this._fuel += (curr <= 100 - this._param.charge) ? this._param.charge : 100 - curr;
+};
+
+Ship.prototype.shift = function(){
+	this._dom.css('transform', 'rotate(' + (this._position += this._param.speed) % 360 + 'deg)');
+};
+
+Ship.prototype.step = function(){
+	if(this._isRunning){
+		
+		if(!this._isCharging){
+			if(this._param.fuelPerStep < this._fuel){
+				//take a step when fuels are enough
+				this.shift();
+				this.consume();
+			}
+			else{
+				this._isCharging = true;
+			}
+		}
+		else{
+			if(this._fuel > 50){
+				this._isCharging = false;
+			}
+		}
+	}
+};
+
 Ship.prototype.move = function(){
 	
-	this._dom.css('transform', 'rotate(' + (this._position += this._speed) % 360 + 'deg)');
+	if(!this._isRunning){
+		this._isRunning = true;
+		var self = this;
+		var step = function(){
+			self.step();
+		}
+		this._fd = setInterval(step, this._param.stepInterval);
+	}
 };
 
 Ship.prototype.stop = function(){
+	this._isRunning = false;
+	clearInterval(this._fd);
+};
 
+Ship.prototype.exec = function(cmd){
+	if(cmd.id === this._id){
+		this[cmd.command].call(this);
+		return true;
+	}
+	return false;
 }
 
 /*
 
-|--- commander --- shipId
-              |*** launch()
-              |*** moveCmd()
-              |*** stopCmd()
-              |*** destroyCmd()
+|--- commander ---|
 
 */
 
-/*
+function Commander(space){
+	this._dom = $('.cmd');
+	this.ships = {};
+	this.shipCnt = 0;
+	this.max = 4;
+};
 
-|--- mediator *** recieve()
-             |*** forward()
+Commander.prototype.template = 
+			 "<label>对第{}号飞船下达命令</label>" + 
+			 "<button name='{}' class='cmd-move'>飞行</button>" + 
+			 "<button name='{}' class='cmd-stop'>停止</button>" +
+			 "<button name='{}' class='cmd-destroy'>销毁</button>";
 
-*/
+Commander.prototype.command = function(btn, mediator){
+	var id = parseInt(btn.attr('name'));
+	var cmd = {};
+	cmd.id = id;
+	if(btn.hasClass('cmd-create')){
+		this.create(mediator);
+		return;
+	}
+	else if(btn.hasClass('cmd-move')){
+		cmd.command = 'move';
+	}
+	else if(btn.hasClass('cmd-stop')){
+		cmd.command = 'stop';
+	}
+	else if(btn.hasClass('cmd-destroy')){
+		cmd.command = 'destroy';
+		this.destroy(id);
+	}
+	mediator.getCommand(cmd);
+};
 
-/*
+Commander.prototype.destroy = function(id){
+	this.ships[id].detach();
+	delete this.ships[id];
+	this.shipCnt--;
+}
 
-|--- space view(coordinate system) --- ships
-
-*/
-
-var Universe = (function(){
-	var instance;
-
-	var Universe = function(){
-		if(instance){
-			return instance;
+Commander.prototype.create = function(mediator){
+	var code = 0;
+	if(this.shipCnt < this.max){
+		for(var i = 0; i < this.max; i++){
+			if(!this.ships[i]){
+				code = i;
+				break;
+			}
 		}
 
-		this.ships = [];
-		this.space = this.init();
+		var dom = $$('div').html(this.template.replace(/\{\}/g, code));
 
-		instance = this;
-	};
+		this._dom.prepend(dom);
 
-	Universe.prototype.init = function(){
-		var space = $$('div').addClass('space');
-		var planet = $$('div').addClass('planet');
+		var newShip = new Ship(code);
+		mediator.register(newShip);
+		this.ships[code] = dom;
+		this.shipCnt++;
+	}
+	else{
+		alert('No more than 4 ships can be created!');
+	}
+};
 
-		planet.appendTo(space);
-		space.appendTo($(document.body));
-		return space;
-	};
 
-	return Universe;
-})();
+/*
+
+|--- mediator ---|
+
+*/
+
+function Mediator(){
+	this.ships = [];
+	this._console = $('.console');
+}
+
+Mediator.prototype.getCommand = function(cmd){
+	var rand = Math.random();
+	var command = cmd.command;
+	var r = false;
+	if(rand > 0.7 && rand <= 1.0){
+		var t = -1;
+		for(var i = 0; i < this.ships.length; i++){
+			var r = this.ships[i].exec(cmd);
+			if(command === 'destroy' && r){
+				t = i;
+			}
+		}
+		if(t > -1){
+			this.ships = this.ships.slice(0, t)
+							   	   .concat(this.ships.slice(t + 1));
+		}
+		$$('p').html("命令" + cmd.command + ": " + cmd.id + "传送成功").appendTo(this._console);
+	}
+	else{
+		$$('p').html("命令" + cmd.command + ": " + cmd.id + "丢失").appendTo(this._console);
+	}
+}
+
+Mediator.prototype.register = function(ship){
+	this.ships.push(ship);
+}
 
 /***** main *****/
 
-var p1 = new Universe();
-var p2 = new Universe();
+//////////////////
 
-console.log(p1 === p2);
+var Singleton = {
+	Commander: Commander,
+	Mediator: Mediator,
+	getInstance: function(type){
+		if(typeof this[type] === 'function'){
+			this[type] = new this[type]();
+		}
+		return this[type];
+	}
+}
 
-var s = new Ship(p1.space);
+function init(){
+	var commander = Singleton.getInstance('Commander');
+	var mediator = Singleton.getInstance('Mediator');
+	
+	$('.cmd').delegate('button', 'click', (function(){
+		var com = commander;
+		var med = mediator;
+		return function(){
+			com.command($(this), med);
+		}
+	})());
+}
 
-setInterval(function(){
-	s.move();
-}, 10);
+init();
+
+//////////////////
