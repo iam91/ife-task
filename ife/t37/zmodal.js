7c56d5f7a24b6a5b055256f12348dee64a6d6b9b
iam91 <zhengwy91@foxmail.com>
2016-10-18 13:58:49 +0800

    update

--- ife/t37/zmodal.js
+++ ife/t37/zmodal.js
@@ -33,39 +33,39 @@
 	function ZModal(base){
 		this._base = base;
 		this._content = null;
-		this._preX = 0;
-		this._preY = 0;
+		this._innerOffsetX = 0;
+		this._innerOffsetY = 0;
 		this._randomClickHandler = null;
-		this._mousemoveHandler = null;
-		this._mousedownHandler = null;
-		this._mouseupHandler = null;
+		this._dragHandler = null;
+		this._dragStartHandler = null;
+		this._dragStopHandler = null;
 		this._init();
 	}
 
 	ZModal.prototype._init = function(){
 		this._content = this._base.querySelector('.z-modal-content');
 		addHandler(this._base, 'click', this._randomClickHandler = this._handlerWrapper(this._randomClick));
-		addHandler(this._content, 'mousedown', this._mousedownHandler = this._handlerWrapper(this._mousedown));
-		addHandler(this._content, 'mouseup', this._mouseupHandler = this._handlerWrapper(this._mouseup));
+		addHandler(this._content, 'mousedown', this._dragStartHandler = this._handlerWrapper(this._dragStart));
+		addHandler(this._content, 'mouseup', this._dragStopHandler = this._handlerWrapper(this._dragStop));
 	};
 
-	ZModal.prototype._mousemove = function(e){
-		this._content.style.left = e.pageX - this._preX + 'px';
-		this._content.style.top = e.pageY - this._preY + 'px';
+	ZModal.prototype._drag = function(e){
+		this._content.style.left = e.pageX - this._innerOffsetX + 'px';
+		this._content.style.top = e.pageY - this._innerOffsetY + 'px';
 	};
 
-	ZModal.prototype._mousedown = function(e){
+	ZModal.prototype._dragStart = function(e){
 		this._content.style.left = this._content.offsetLeft + 'px';
 		this._content.style.top = this._content.offsetTop + 'px';
 		this._content.style.margin = '0';
-		this._preX = e.pageX - this._content.offsetLeft;
-		this._preY = e.pageY - this._content.offsetTop;
-		addHandler(this._content, 'mousemove', this._mousemoveHandler = this._handlerWrapper(this._mousemove));
+		this._innerOffsetX = e.pageX - this._content.offsetLeft;
+		this._innerOffsetY = e.pageY - this._content.offsetTop;
+		addHandler(this._content, 'mousemove', this._dragHandler = this._handlerWrapper(this._drag));
 	};
 
-	ZModal.prototype._mouseup = function(e){
-		removeHandler(this._content, 'mousemove', this._mousemoveHandler);
-		this._mousemoveHandler = null;
+	ZModal.prototype._dragStop = function(e){
+		removeHandler(this._content, 'mousemove', this._dragHandler);
+		this._dragHandler = null;
 	}
 
 	ZModal.prototype._randomClick = function(e){
