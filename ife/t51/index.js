fcbb700b98269c09f3078df1b5e7d7b8616f49e3
iam91 <zhengwy91@foxmail.com>
2016-10-28 17:16:31 +0800

    update

--- ife/t51/index.js
+++ ife/t51/index.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 	var urls = [
 			'https://placehold.it/350x150/b81d18',
-			'https://placehold.it/300x150/202020'/*,
+			'https://placehold.it/300x150/202020',
 			'https://placehold.it/250x250/004687',
 			'https://placehold.it/250x150/b6b6b6',
 			'https://placehold.it/350x150/f0f0f0',
@@ -17,9 +17,12 @@ window.onload = function(){
 			'https://placehold.it/350x450/ff5a09',
 			'https://placehold.it/350x250/b81d18',
 			'https://placehold.it/350x450/b6b6b6',
-			'https://placehold.it/350x350/004687'*/];
+			'https://placehold.it/350x350/004687'];
 
 	var gg = new g(document.querySelector('.z-gallery'));
 	console.log(gg);
+	gg.setLayout(gg.LAYOUT.BRICK);
+	gg.setGutter(10, 15);
+	gg.init({colCount: 3});
 	gg.setImage(urls);
 };
--- ife/t51/js/zgallery.js
+++ ife/t51/js/zgallery.js
@@ -77,9 +77,6 @@
 				var y = hh / h;
 				img.classList.add(ClassName.V_CLIP);
 				var polygon = '<polygon points="' + x + ' 0, 1 0, 1 ' + y + ', 0 ' + y + '"/>';
-				/*
-				img.setAttribute('style', 
-					'-webkit-clip-path: polygon(' + x + ' 0, 100% 0, 100%' + y + '%, 0 ' + y + '%);');*/
 			}
 			else{
 				//too much width
@@ -94,9 +91,6 @@
 				var x2 = ww / w;
 				img.classList.add(ClassName.H_CLIP);
 				var polygon = '<polygon points="' + x1 + ' 0, ' + x2 + ' 0, ' + x2 + ' 1, 0 1"/>';
-				/*
-				img.setAttribute('style', 
-					'-webkit-clip-path: polygon(' + 100*x1 + '% 0, ' + 100*x2 + '% 0, ' + 100*x2 + '% 100%, 0 100%);');*/
 			}
 			var styleInner = '.' + ClassName.JIGSAW_X + count + " div:nth-child(" + (index + 1) + ") img{-webkit-clip-path: url('#clip-shape-" + index + "');clip-path: url('#clip-shape-" + index + "');}";
 
@@ -104,7 +98,6 @@
 			var t = document.createElement('div');
 			var s = document.createElement('style');
 			t.innerHTML = svgHead + polygon + svgTail;
-			console.log(polygon);
 			s.innerHTML = styleInner;
 			s.type = "text/css";
 			document.body.appendChild(t);
@@ -117,11 +110,14 @@
 	 */
 	var ClassName = {
 		JIGSAW_G: 'z-g-jigsaw',
-		WATERFALL_G: 'z-g-waterfall',
-		BRICK_G: 'z-g-brick',
 		JIGSAW_X: 'z-g-jigsaw-',
+		WATERFALL_G: 'z-g-waterfall',
+		WATERFALL_COL: 'z-g-waterfall-col',
+		WATERFALL_PIC: 'z-g-waterfall-pic',
+		BRICK_G: 'z-g-brick',
+		BRICK_ROW: 'z-g-brick-row',
 		V_CLIP: 'v-clip',
-		H_CLIP: 'h-clip'
+		H_CLIP: 'h-clip',
 	};
 
 	/**
@@ -150,18 +146,38 @@
 	function ZGallery(g){
 
 		/**
+		 * Common member variables
 		 * @private
 		 */
 		this._g = g;
-
-		this._gutterY = 0;
-		this._gutterX = 0;
 		this._layout = this.LAYOUT.JIGSAW;
 		this._urls = [];
 		this._urlIndex = 0;
 		this._imageElements = [];
-
 		this._cache = [];
+
+		this._initialized = false;
+
+		this._gutterX = 0;
+		this._gutterY = 0;
+
+		/**
+		 * Waterfall layout member variables
+		 * @private
+		 */
+		this._colCount = 4;
+		this._cols = null;
+
+		/**
+		 * Brick layout member variables
+		 * @private
+		 */
+		this._maxBin = 6;
+		this._minBin = 3;
+		this._minHeight = 150;
+		//this._maxHeight
+		this._cacheHead = 0;
+		this._cacheTail = 1;
 	}
 
 	/******* The following are event handlers *******/
@@ -174,18 +190,8 @@
 		this._fetchImage();
 		var img = e.target;
 		this._cache.push(img);//console.log(this._cache)
-
-		switch(this._layout){
-			case this.LAYOUT.JIGSAW:
-				this._placeImageJigsaw();
-				break;
-			case this.LAYOUT.WATERFALL:
-				break; 
-			case this.LAYOUT.BRICK:
-				break;
-			default:
-				break;
-		};
+		this._cacheTail++;
+		this._placeImage();
 	};
 
 	/**
@@ -196,20 +202,11 @@
 		this._fetchImage();
 		var img = e.target;
 		this._cache.push(img);
-
-		switch(this._layout){
-			case this.LAYOUT.JIGSAW:
-				break;
-			case this.LAYOUT.WATERFALL:
-				break; 
-			case this.LAYOUT.BRICK:
-				break;
-			default:
-				break;
-		};
+		this._cacheTail++;
+		this._placeImage();
 	};
 
-	/******* The following are private methods *******/
+	/******* The following are private methods for common use *******/
 	
 	ZGallery.prototype._fetchImage = function(){
 		if(this._urlIndex < this._urls.length){
@@ -223,18 +220,31 @@
 		}
 	};
 
-	ZGallery.prototype._placeImageJigsaw = function(){
-		this._g.classList.add(ClassName.JIGSAW_G);
+	ZGallery.prototype._placeImage = null;
 
-		if(this._cache.length == this._urls.length   //images are all loaded 
+	/******* The following are private methods for a speicific layout *******/
+
+	ZGallery.prototype._initJigsaw = function(){
+		this._g.classList.add(ClassName.JIGSAW_G);
+		//fixed the height
+		this._g.style.height = this._g.clientWidth * GlobalConst.jigsaw.ratio + 'px';
+
+		function jigsawOnresize(e){
+			this._g.style.height = this._g.clientWidth * GlobalConst.jigsaw.ratio + 'px';
+		}
+
+		Util.addHandler(window, 'resize', Util.eventHandlerWrapper(jigsawOnresize, this, false));
+	};
+
+	ZGallery.prototype._placeImageJigsaw = function(){
+
+		if(this._cache.length == this._urls.length                //images are all loaded 
 			|| this._cache.length >= GlobalConst.jigsaw.max){     //jigsawMax has reached
 			if(this._g.innerHTML){
 				//layout should be reset
 				this._g.innerHTML = '';
 			}
-
-			//fixed the height
-			this._g.style.height = this._g.clientWidth * GlobalConst.jigsaw.ratio + 'px';
+			
 			var count = GlobalConst.jigsaw.max < this._cache.length ? GlobalConst.jigsaw.max : this._cache.length;
 			this._g.classList.add(ClassName.JIGSAW_X + count);
 
@@ -256,16 +266,109 @@
 				div.appendChild(img);
 				this._g.appendChild(div);
 			}
-
-			/**
-			 * @todo add resize handler
-			 */
 		}
 	};
 
-	ZGallery.prototype._placeImageWaterfall = function(){};
+	ZGallery.prototype._initWaterfall = function(opt){
+		this._g.classList.add(ClassName.WATERFALL_G);
+		this._colCount = opt && opt.colCount || 4;
+		this._cols = new Array(this._colCount);
 
-	ZGallery.prototype._placeImageBrick = function(){};
+		//Column width in percentage relative to container.
+		var colWidth = 100 / this._colCount;
+		//Render the column containers.
+		var frag = document.createDocumentFragment();
+		for(var i = 0; i < this._colCount; i++){
+			var col = document.createElement('div');
+			col.style.width = colWidth + '%';
+			col.classList.add(ClassName.WATERFALL_COL);
+			frag.appendChild(col);
+			this._cols[i] = col;
+		}
+		this._g.appendChild(frag);
+	};
+
+	ZGallery.prototype._placeImageWaterfall = function(){
+		var img = this._cache[this._cache.length - 1];
+		var div = document.createElement('div');
+		div.appendChild(img);
+		div.classList.add(ClassName.WATERFALL_PIC);
+		//Set horizontal gutter width.
+		div.style.paddingBottom = this._gutterY + 'px';
+
+		//Find the shortest column.
+		var minIndex = 0;
+		var minReferer = this._cols[minIndex];
+		var min = minReferer.clientHeight;
+		for(var i = 0; i < this._colCount; i++){
+			var h = this._cols[i].clientHeight;
+			if(min > h){
+				min = h;
+				minIndex = i;
+				minReferer = this._cols[minIndex];
+			}
+		}
+		if(minIndex < this._colCount - 1){
+			//Set vertical gutter width.
+			div.style.paddingRight = this._gutterX + 'px';
+		}
+		minReferer.appendChild(div);
+	};
+
+	ZGallery.prototype._initBrick = function(opt){
+		this._g.classList.add(ClassName.BRICK_G);
+
+		this._minBin = opt && opt.minBin || 3;
+		this._maxBin = opt && opt.maxBin || 6;
+		this._minHeight = opt && opt.minHeight || 150;
+		this._cacheHead = 0;
+		this._cacheTail = 1;
+	};
+
+	ZGallery.prototype._placeImageBrick = function(){
+		var cacheWindow = this._cacheTail - this._cacheHead + 1;
+
+		if(cacheWindow >= this._maxBin){
+			//layout algorithm
+			var totWidth = this._g.clientWidth;
+			var totImageWidth = 0;
+			var id = this._cacheHead;
+			while(id < this._cacheTail){
+				var img = this._cache[id];
+				var w = this._minHeight * img.width / img.height;
+				if(
+					totImageWidth + w > totWidth 
+					&& (id - this._cacheHead + 1) > this._minBin  //must be more than minBin
+				   )
+				{
+					break;
+				}
+				id++;
+				totImageWidth += w;
+			}
+
+			var styleHeight = this._minHeight //* totWidth / totImageWidth;
+			var row = document.createElement('div');
+			row.classList.add(ClassName.BRICK_ROW);
+			for(var i = this._cacheHead; i < id; i++){
+				var img = this._cache[i];
+				
+				this._cacheHead++;
+
+				var wiggle = 0.95;
+				var styleWidth = this._minHeight * img.width / img.height / totImageWidth;
+				//var styleWidth = styleHeight * img.width / img.height * wiggle ;
+
+				var imgWrapper = document.createElement('div');
+				imgWrapper.style.width = styleWidth * 100 + '%';//styleWidth + 'px';
+				//imgWrapper.style.height = styleHeight + 'px';
+
+				imgWrapper.appendChild(img);
+				row.appendChild(imgWrapper);
+			}
+			this._g.appendChild(row);
+		}
+	};
 
 	/******* The following are public methods and variables provided by zgallery *******/
 
@@ -287,6 +390,11 @@
 	 * @param {(string|string[])} image A single image url or an array of image urls.
 	 */
 	ZGallery.prototype.setImage = function(image){
+		if(!this._initialized){
+			console.warn('ZGallery has not been initialized yet!');
+			return;
+		}
+
 		if(typeof image === 'string'){
 			this.setImage([image]);
 			return;
@@ -300,6 +408,16 @@
 		this._urls = Util.arrayClone(this._urls, image);
 		this._fetchImage();
 	};
+
+	/**
+     * Set the gutter width between images.
+     * @param {number}  x  The vertical gutter width between images.
+     * @param {number} [y] The horizontal gutter width between images, it equals to x if y is undefined. 
+     */
+    ZGallery.prototype.setGutter = function (x, y) {
+    	this._gutterX = x || 0;
+    	this._gutterY = y || this._gutterX;
+    };
 
 	/**
 	 * Set the layout of zgallery.
@@ -320,6 +438,36 @@
 	 */
 	ZGallery.prototype.getLayout = function(){
 		return this._layout;
+	};
+
+	/**
+	 * Initialize zgallery based on specified layout.
+	 * @public
+	 * @param {object} opt Configuration options for zgallery.
+	 */
+	ZGallery.prototype.init = function(opt){
+		if(this._initialized){
+			return;
+		}
+
+		switch(this._layout){
+			case this.LAYOUT.JIGSAW:
+				this._initJigsaw(opt);
+				this._placeImage = this._placeImageJigsaw;
+				break;
+			case this.LAYOUT.WATERFALL:
+				this._initWaterfall(opt);
+				this._placeImage = this._placeImageWaterfall;
+				break; 
+			case this.LAYOUT.BRICK:
+				this._initBrick(opt);
+				this._placeImage = this._placeImageBrick;
+				break;
+			default:
+				break;
+		};
+
+		this._initialized = true;
 	};
 
 	window.g = ZGallery;
