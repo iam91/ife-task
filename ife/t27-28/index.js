3c7cc0aca30af381d5f4e31dd102f7fd05d2156b
iam91 <zhengwy91@foxmail.com>
2016-11-03 21:05:42 +0800

    clear

--- ife/t27-28/index.js
+++
@@ -1,7 +0,0 @@
-var $$ = function(element){
-	//convert newly created dom object to jquery object
-	return $(document.createElement(element));
-};
-
-var sysTimeInterval = 50;
-var timeGra = 1000 / sysTimeInterval;
